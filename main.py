48db546a78a4ec6b7bfd2a107cca4d2dcbe4cb38
hoyoung <hoyoung120873@gmail.com>
2026-05-09 21:02:56 +0900

    레퍼지토리 정리

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from yolo-a1!")
+from ultralytics import YOLO
+import os
 
+def main():
+    model_path = "model/eyesyawn.pt"
+    
+    if not os.path.exists(model_path):
+        print(f"{model_path} 경로에 모델 파일이 없습니다.")
+        return
+
+    # 2. 모델 로드
+    model = YOLO(model_path)
+
+    results = model.predict(
+        source="data/test/images",
+        save=True,                 
+        conf=0.25,                 
+        line_width=2               
+    )
 
 if __name__ == "__main__":
     main()
